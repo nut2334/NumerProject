a3a1d0f6162b6420d09ce57d3dcce10adb092235
nut2334 <nut2334@gmail.com>
2023-04-08 02:29:09 +0700

    Cramer 70%

--- src/Cramer-Rule.tsx
+++ src/Cramer-Rule.tsx
@@ -1,27 +1,48 @@
 import React, { useState } from 'react'
 import { Button } from '@mui/material';
+import {det} from 'mathjs';
 
 const CramerRule = () => {
     const [numInputs, setNumInputs] = useState(0);
 
     const inputElements: JSX.Element[] = [];
     const inputElements2: JSX.Element[] = [];
+    const A: number[][] = [];
+    const B: number[] = [];
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const inputCount = parseInt(event.target.value);
         setNumInputs(inputCount);
     }
 
-    for (let i = 1; i <= numInputs * numInputs; i++) {
-        inputElements.push(<input key={i} />);
-        if (i % numInputs == 0) {
-            inputElements.push(<br />)
-        }
+    const inputA = (event: React.ChangeEvent<HTMLInputElement>, i: number ,j: number) => {
+        const value = Number(event.target.value);
+        A[i-1][j-1]=value;
+        console.log(A);
     }
+    const inputB = (event: React.ChangeEvent<HTMLInputElement>, i: number) => {
+        B[i-1]=Number(event.target.value);
+        console.log(B);
+    }
+
     for (let i = 1; i <= numInputs; i++) {
-        inputElements2.push(<input key={i} />);
+        for(let j = 1; j <= numInputs; j++){
+            inputElements.push(<input onChange={event=> inputA(event, i,j)}/>);
+            if (j % numInputs == 0) {
+                A[i-1]=[];
+                inputElements.push(<br />);
+            }
+        }
+    }   
+    for (let i = 1; i <= numInputs; i++) {
+        inputElements2.push(<input onChange={event=> inputB(event, i)} />);
         inputElements2.push(<br />);
     }
+    const Calculate = () => {
+        const determinant = det(A);
+        console.log('Determinant:', determinant);
+    }
+    
 
     return (
         <div style={{ textAlign: "center" }}>
@@ -31,9 +52,7 @@ const CramerRule = () => {
             <div style={{ display: "inline-block" }}>{inputElements}</div>&emsp;&emsp;
             <div style={{ display: "inline-block" }}>{inputElements2}</div>
             <br />
-            <Button variant="contained" sx={{ m: 2 }}>
-                Calculate
-            </Button>
+            <Button variant="contained" sx={{ m: 2 }} onClick={Calculate} >Calculate</Button>
         </div>
     )
 }
